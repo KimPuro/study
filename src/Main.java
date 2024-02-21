1a73b77accb2b43102820efb160bf590573a24c0
kimpuro <puro@protonmail.ch>
2024-02-21 18:10:24 +0900

    2.22 update

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-package src;
-public class Main {
-    public static void main(String[] args) {
-        var calculator = new SwitchCalc();
-        calculator.calculate();
-        System.out.println("계산 결과 : " + calculator.getResult());
-    }
-}
--- src/kaupTest/KaupMain.java
+++
@@ -1,9 +0,0 @@
-package src.kaupTest;
-
-public class KaupMain {
-    public static void main(String[] args) {
-        double m = (Math.random() * 50.0 + 150.0)/100.0;
-        double kg = Math.random() * 100.0 + 30.0;
-        System.out.printf("BMI : %.1f", kg/(m*m));
-    }
-}
